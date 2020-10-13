99e6cfa82383e578452492ad474f6dbc3b1aedc3
Illya Antonuk <illantalex84@gmail.com>
2020-10-13 13:30:33 +0300

    added an app mychat, fixed security issues

--- mychat/mychat/settings.py
+++ mychat/mychat/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "n-vm-$1&vmx^z6t+cxag(jh(d_*n72=yu44#j_y$$o=%ql8sl&"
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
